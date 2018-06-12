bd64c755d4858c8608dd913f366b787ffa4ce54f
Owen <zouwen1213@126.com>
2018-06-12 08:13:34 +0800

    1. 提交初始代码

---
+++ shoujiduan03/frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : E:\RY\shoujiduan03\frameworks\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml \
---
+++ shoujiduan03/frameworks/cocos2d-x/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
@@ -0,0 +1,3 @@
+E:\RY\shoujiduan03\frameworks\cocos2d-x\cocos\platform\android\java\gen\com\enhance\gameservice\IGameTuningService.java: \
+  E:\RY\shoujiduan03\frameworks\cocos2d-x\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl 
+
