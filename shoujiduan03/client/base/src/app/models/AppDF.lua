b5c8d150de05d5a6ac024de2d002626c8ce250ab
lufthansa <lufthansa@163.com>
2019-05-06 23:30:45 +0800

    sheng ji version

--- shoujiduan03/client/base/src/app/models/AppDF.lua
+++ shoujiduan03/client/base/src/app/models/AppDF.lua
@@ -24,13 +24,10 @@ appdf.DOWN_ERROR_CREATEURL					= 6 									--创建连接失败
 appdf.DOWN_ERROR_NET		 				= 7 									--下载失败
 
 --程序版本
-appdf.BASE_C_VERSION = 0 --@app_version
+appdf.BASE_C_VERSION = 4 --@app_version
 --资源版本
-<<<<<<< HEAD
-appdf.BASE_C_RESVERSION = 5 --@client_version
-=======
-appdf.BASE_C_RESVERSION = 6 --@client_version
->>>>>>> 7de21a8ea8ed94648755102c74798136b56318f1
+appdf.BASE_C_RESVERSION = 7 --@client_version
+
 appdf.BASE_GAME =
 {
 	--{kind = 6,version = "0"}
